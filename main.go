a09c8471c0511efada1522facadb22ee60f91cdd
Brad McCormack <bradmccormack100@gmail.com>
2017-10-29 16:09:31 +1100

    Add SSL support to the library

--- main.go
+++ main.go
@@ -22,17 +22,79 @@ import (
 var mess = &Messenger{}
 
 func main() {
-	port := os.Getenv("PORT")
-	log.Println("Server start in port:", port)
-	mess.VerifyToken = os.Getenv("TOKEN")
-	mess.AccessToken = os.Getenv("TOKEN")
-	log.Println("Bot start in token:", mess.VerifyToken)
+
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		log.Fatalf("Please specify a PORT to bind on")
+	}
+	portStr := fmt.Sprintf(":%s", port)
+
+	log.Println("Listening on port :", port)
+
+	certfile, ok := os.LookupEnv("CERTFILE")
+	if !ok {
+		log.Fatalf("Please specify a CERTFILE")
+	}
+	log.Println("Using certfile :", certfile)
+
+	keyfile, ok := os.LookupEnv("KEYFILE")
+	if !ok {
+		log.Fatalf("Please specify a KEYFILE to bind on")
+	}
+
+	log.Println("Using keyfile ", keyfile)
+
+	token, ok := os.LookupEnv("TOKEN")
+
+	if !ok {
+		log.Fatalf("Please specify a TOKEN for use with the Facebook Messenger API")
+	}
+
+	mess.VerifyToken = token
+	mess.AccessToken = token
+	log.Println("Using token", token)
+
 	mess.MessageReceived = MessageReceived
-	http.HandleFunc("/webhook", mess.Handler)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world"))
+	})
+
+	// Register FB Messenger webhook with the messanger class handler routine
+	http.HandleFunc("/webhook/", mess.Handler)
+
+	/* TODO automatic renewal for Let's encrypt
+
+	domain, ok := os.LookupEnv("DOMAIN")
+	if !ok {
+		log.Fatalf("Please specify a DOMAIN to use with the certManager (to organise TLS for you)")
+	}
+
+	log.Printf("Using domain %s for certmanager", domain)
+
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist(domain),
+		Cache:      autocert.DirCache("certs"),
+	}
+
+
+	server := &http.Server{
+		Addr: portStr,
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
+	}
+	server.ListenAndServeTLS("", "") //key and cert are comming from Let's Encrypt
+
+	*/
+
+	// func ListenAndServeTLS(addr, certFile, keyFile string, handler Handler) error
+	log.Fatal(http.ListenAndServeTLS(portStr, certfile, keyfile, nil))
+
 }
 
-//MessageReceived :Callback to handle when message received.
+// MessageReceived :Callback to handle when message received.
 func MessageReceived(event Event, opts MessageOpts, msg ReceivedMessage) {
 	// log.Println("event:", event, " opt:", opts, " msg:", msg)
 	profile, err := mess.GetProfile(opts.Sender.ID)
